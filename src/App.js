388f87c8c883a9b5136bd4702ba6280c74051c68
chirantar <gupta.chirantar@gmail.com>
2023-04-30 19:08:24 +0530

    Style changes.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import {useTypingText} from "./useTypingText";
 
 function App() {
 
-    const { word } = useTypingText(['Hi', 'Hello', 'whatsup'], 130, 20);
+    const { word } = useTypingText(['hiiii', 'hello', 'whatsup', 'how are are you'], 130, 20);
 
     return (
         <div className="App">
             <p>
-                <h1>No {word}</h1>
+                <h1 className="header1">No <strike className="strike">{word}</strike></h1>
+                <h6 className="header6">No need to call me🤦‍♀, I am already taken️</h6>
             </p>
         </div>
     );
